785a55be1bdabadc876dcff80a8c90d8cfe8d943
ogogtryIntelJ <paksutan2580@gmail.com>
2022-05-23 14:49:00 +0900

    javaScripts練習

---
+++ .idea/javaScriptStudy.js
@@ -0,0 +1,5 @@
+
+//html のid"test"から取得した情報をlet displayに代入する
+let display = document.getElementById("test");
+//titleの情報を取得、変更するのは下記、やっていることはもともとのタイトルに（１）をつけているだけ
+document.title = "(1)" + document.title
